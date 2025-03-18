2fba19525fb5632f9a92caf06282f97a9d8830ab
Dima Komisarov <komissarov854@gmail.com>
2025-03-18 22:07:56 +0200

    make build

---
+++ index.7158fd81.js
@@ -0,0 +1,2 @@
+var e=new Promise(function(e){document.querySelector(".logo").addEventListener("click",e)}),t=new Promise(function(e,t){setTimeout(function(){return t(Error("error"))},3e3)}),n=function(){var e=document.createElement("div");e.className="message",e.textContent="Promise was resolved!",document.body.appendChild(e)},o=function(){var e=document.createElement("div");e.className="message error-message",e.textContent="Promise was rejected!",document.body.appendChild(e)};e.then(n).catch(o),t.then(n).catch(o);
+//# sourceMappingURL=index.7158fd81.js.map
